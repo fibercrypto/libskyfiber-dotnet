7bbc17ea504a811de5cc53a6f17ee06c0349237b
Maykel Arias Torres <dev0006@simelo.tech>
2018-08-24 15:46:34 -0400

    [libnet][swig] refs #4 Testing request in travis

--- LibskycoinNetTest/LibskycoinNetTest.cs
+++ LibskycoinNetTest/LibskycoinNetTest.cs
@@ -14,8 +14,10 @@ namespace LibskycoinNetTest
 
         [Test]
 		public void SumTest(){
-
-			Assert.AreEqual(0, 0);
+			var xyz = new skycoin.secp256k1go__XYZ();
+			var xy = new skycoin.secp256k1go__XY();
+			uint error = skycoin.skycoin.SKY_secp256k1go_XYZ_SetXY(xyz, xy);
+			Assert.AreEqual(error, 0);
 		}
     }
 
