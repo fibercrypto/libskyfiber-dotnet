e968f32ffda392a4cd604f73107ae7e127358064
stdevYuniers <dev0010@simelo.tech>
2018-12-01 01:00:12 -0500

    [ci] refs #6 Add install msbuild package

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -3,9 +3,9 @@
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
     sudo apt-key adv --no-tty --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    sudo apt-get install apt-transport-https
     sudo echo "deb https://download.mono-project.com/repo/ubuntu wheezy/snapshots/$MONO_VERSION main" > /etc/apt/sources.list.d/mono-official-stable.list
     sudo dpkg -i packages-microsoft-prod.deb
-    sudo apt-get install apt-transport-https
     sudo apt-get update
     sudo apt-get upgrade
     sudo apt-get install -y \
@@ -13,7 +13,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
                  mono-devel \
                  mono-complete \
                  referenceassemblies-pcl \
-                 ca-certificates-mono
+                 ca-certificates-mono \
+                 msbuild
 fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
