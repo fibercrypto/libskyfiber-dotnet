0223bdf2030da16ab7aec6cfbe13c94aa4db3f7d
Olemis Lang <olemis+work@gmail.com>
2018-12-08 13:54:05 -0500

    [ci] refs #6 - Inverse logic in installpkg.sh script. Wrong before

--- .travis/installpkg.sh
+++ .travis/installpkg.sh
@@ -1,7 +1,7 @@
 
 echo "Installing $1 at version $2 in folder $3"
 
-if [ "$DOTNETCORE" == "" ]; then
+if [ "$DOTNETCORE" != "" ]; then
   dotnet add package $1 --version $2 --package-directory $3
 else
   nuget install $1 -Version $2 -OutputDirectory $3
