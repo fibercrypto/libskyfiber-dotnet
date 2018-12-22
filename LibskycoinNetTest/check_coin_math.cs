2f108fa4fc9dc7de8c3415201a4301b493a6592e
Maykel Arias Torres <dev0006@simelo.tech>
2018-12-22 09:19:30 -0500

    [swig] refs #6 Changes test math

--- LibskycoinNetTest/check_coin_math.cs
+++ LibskycoinNetTest/check_coin_math.cs
@@ -4,17 +4,17 @@ using skycoin;
 using utils;
 namespace LibskycoinNetTest {
     [TestFixture ()]
-    public class check_coin_math : skycoin.skycoin {
+    public class check_coin_math  {
 
         utils.transutils transutils = new utils.transutils ();
         [Test]
         public void TestAddUint64 () {
-            var r = new_GoUint64p ();
-            var err = SKY_coin_AddUint64 (10, 11, r);
-            Assert.AreEqual (err, SKY_OK);
-            Assert.AreEqual (GoUint64p_value (r), 21);
-            err = SKY_coin_AddUint64 (ulong.MaxValue, 1, r);
-            Assert.AreEqual (err, SKY_ErrUint64AddOverflow);
+            var r = skycoin.skycoin.new_GoUint64p ();
+            var err = skycoin.skycoin.SKY_coin_AddUint64 (10, 11, r);
+            Assert.AreEqual (err, skycoin.skycoin.SKY_OK);
+            Assert.AreEqual (skycoin.skycoin.GoUint64p_value (r), 21);
+            err = skycoin.skycoin.SKY_coin_AddUint64 (ulong.MaxValue, 1, r);
+            Assert.AreEqual (err, skycoin.skycoin.SKY_ErrUint64AddOverflow);
         }
         struct math_test {
             public ulong a;
@@ -28,35 +28,35 @@ namespace LibskycoinNetTest {
             var c = new math_test ();
             c.a = 0;
             c.b = 0;
-            c.failure = SKY_OK;
+            c.failure = skycoin.skycoin.SKY_OK;
             cases[0] = c;
 
             c = new math_test ();
             c.a = 1;
             c.b = 1;
-            c.failure = SKY_OK;
+            c.failure = skycoin.skycoin.SKY_OK;
             cases[1] = c;
 
             c = new math_test ();
             c.a = long.MaxValue;
             c.b = long.MaxValue;
-            c.failure = SKY_OK;
+            c.failure = skycoin.skycoin.SKY_OK;
             cases[2] = c;
 
             c = new math_test ();
             c.a = ulong.MaxValue;
             c.b = 0;
-            c.failure = SKY_ERROR;
+            c.failure = skycoin.skycoin.SKY_ERROR;
             cases[3] = c;
         }
 
         [Test]
         public void TestUint64ToInt64 () {
             for (int i = 0; i < cases.Length; i++) {
-                var r = new_Gointp ();
-                var err = SKY_coin_Uint64ToInt64 (cases[i].a, r);
+                var r = skycoin.skycoin.new_Gointp ();
+                var err = skycoin.skycoin.SKY_coin_Uint64ToInt64 (cases[i].a, r);
                 Assert.AreEqual (err, cases[i].failure);
-                Assert.AreEqual (cases[i].b, Gointp_value (r));
+                Assert.AreEqual (cases[i].b, skycoin.skycoin.Gointp_value (r));
             }
         }
     }
