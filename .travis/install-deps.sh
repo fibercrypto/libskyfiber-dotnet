8eb63b95051b4e0f0dc3d2866afd11c8b196ff20
Maykel Arias Torres <dev0006@simelo.tech>
2019-01-11 21:35:27 -0500

    refs #6 Rename RootNamespace LibskycoinNet=>LibSkycoinNet

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-    sudo apt-key adv --no-tty --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
     sudo apt-get install apt-transport-https
-    sudo echo "deb https://download.mono-project.com/repo/ubuntu wheezy/snapshots/$MONO_VERSION main" > /etc/apt/sources.list.d/mono-official-stable.list
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update
 #    sudo apt-get upgrade
@@ -27,9 +27,9 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    curl -o MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg https://download.mono-project.com/archive/4.8.0/macos-10-universal/MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg
+    curl -o MonoFramework-MDK-5.18.0.225.macos10.xamarin.universal.pkg https://download.mono-project.com/archive/5.18.0/macos-10-universal/MonoFramework-MDK-5.18.0.225.macos10.xamarin.universal.pkg
     curl -o dotnet-sdk-2.2.102-osx-gs-x64.pkg https://download.visualstudio.microsoft.com/download/pr/34608266-602a-4f3c-9608-48364b01ac9a/3b890a39949af4592189ca9a300cf651/dotnet-sdk-2.2.102-osx-x64.pkg
-    sudo installer -pkg MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg -target /
+    sudo installer -pkg MonoFramework-MDK-5.18.0.225.macos10.xamarin.universal.pkg -target /
     sudo installer -pkg dotnet-sdk-2.2.102-osx-gs-x64.pkg -target /
 
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
