09b4520b6595c7f7b3f3254a85fd0d6e1d1a3d4e
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-20 16:35:02 -0400

    [travis] Ignore xenial

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -6,7 +6,7 @@ set -ex
 sudo apt update
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-get install apt-transport-https
-echo "deb https://download.mono-project.com/repo/ubuntu vs-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+echo "deb https://download.mono-project.com/repo/ubuntu vs-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 sudo apt update
 sudo apt install apt-transport-https gcc make cmake git -y
 sudo apt-get install monodevelop nuget -y
