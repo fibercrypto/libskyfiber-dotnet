d19048b5dfa08e35838e9a506ec4d2941b80a6a3
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-20 15:36:53 -0400

    [travis] Restore conf

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,4 @@
-nuget push ./lib/swig/LibskycoinNet/bin/Release/*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source $NUGET_SOURCE -SkipDuplicate
 
+mono nuget.exe push ./lib/swig/LibskycoinNet/bin/Release/*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source $NUGET_SOURCE -SkipDuplicate
 (cd ./lib/skyapi/src/Skyapi/ && nuget pack ./Skyapi.nuspec)
-
-nuget push ./lib/skyapi/src/Skyapi/*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source $NUGET_SOURCE -SkipDuplicate
+mono nuget.exe push ./lib/skyapi/src/Skyapi/*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source $NUGET_SOURCE -SkipDuplicate
--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -3,10 +3,9 @@
 set -ex
 
 # Install mono
+echo "deb https://download.mono-project.com/repo/ubuntu vs-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 sudo apt update
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt install apt-transport-https gcc make cmake git -y
-echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
-sudo apt update
 sudo apt-get install monodevelop nuget -y
 
