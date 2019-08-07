f180933bc7d66e8a362517ef10da7eb1fa397a2a
Maykel Arias Torres <dev0006@simelo.tech>
2019-08-07 18:47:41 -0400

    [travis] refs #6 Install dotnet and mono in xenial, and added conf extra in libsky-mono

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -1,18 +1,10 @@
 
-if [[ "$TYPE" == "mono" ]]; then
-    # INstall mono
-    sudo apt update
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    sudo apt install apt-transport-https gcc make cmake git -y
-    echo "deb https://download.mono-project.com/repo/ubuntu vs-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
-    sudo apt update
-    sudo apt-get install monodevelop nuget -y
-fi
-
-if [[ "$TYPE" == "dotnet" ]]; then
-    wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
-    sudo apt-get install apt-transport-https
-    sudo apt-get update
-    sudo apt-get install dotnet-sdk-2.2
-fi
+# INstall mono
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo apt update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt install apt-transport-https gcc g++ make cmake git -y
+sudo dpkg -i packages-microsoft-prod.deb
+echo "deb https://download.mono-project.com/repo/ubuntu vs-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+sudo apt update
+sudo apt-get install monodevelop nuget dotnet-sdk-2.2 -y
