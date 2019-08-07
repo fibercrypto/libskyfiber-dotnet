45785a780416b98fe9c99c89ada09ce1d8adcd72
Maykel Arias Torres <dev0006@simelo.tech>
2019-08-07 18:03:14 -0400

    [ci-script] refs #6 Correcting version the mono and dotnet to install in linux

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -4,13 +4,13 @@ if [[ "$TYPE" == "mono" ]]; then
     sudo apt update
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     sudo apt install apt-transport-https gcc make cmake git -y
-    echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+    echo "deb https://download.mono-project.com/repo/ubuntu vs-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
     sudo apt update
     sudo apt-get install monodevelop nuget -y
 fi
 
 if [[ "$TYPE" == "dotnet" ]]; then
-    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get install apt-transport-https
     sudo apt-get update
