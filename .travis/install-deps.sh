6be21a75f183ed920acf764ef37b338504ad1458
Maykel Arias Torres <dev0006@simelo.tech>
2019-08-07 17:57:44 -0400

    [travis] refs #6 Downgrade version swig 4 => 3.0.12 and dist bionic => xenial

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -14,13 +14,16 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    curl -o MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg https://download.mono-project.com/archive/4.8.0/macos-10-universal/MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg
-    curl -o dotnet-sdk-2.2.102-osx-gs-x64.pkg https://download.visualstudio.microsoft.com/download/pr/34608266-602a-4f3c-9608-48364b01ac9a/3b890a39949af4592189ca9a300cf651/dotnet-sdk-2.2.102-osx-x64.pkg
-    sudo installer -pkg MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg -target /
-    sudo installer -pkg dotnet-sdk-2.2.102-osx-gs-x64.pkg -target /
-
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
-    brew install swig
+    # curl -o MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg https://download.mono-project.com/archive/4.8.0/macos-10-universal/MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg
+    # curl -o dotnet-sdk-2.2.102-osx-gs-x64.pkg https://download.visualstudio.microsoft.com/download/pr/34608266-602a-4f3c-9608-48364b01ac9a/3b890a39949af4592189ca9a300cf651/dotnet-sdk-2.2.102-osx-x64.pkg
+    # sudo installer -pkg MonoFramework-MDK-4.8.0.524.macos10.xamarin.universal.pkg -target /
+    # sudo installer -pkg dotnet-sdk-2.2.102-osx-gs-x64.pkg -target /
+    brew update;
+    echo 'Creating swig@3.0.12 formula';
+    cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core";
+    git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/swig.rb | grep -v 'fails_with' > Formula/swig.rb;
+    echo 'Installing swig@3.0.12 (3.0.12)';
+    brew install swig || brew link --overwrite swig;
     brew install gimme
 fi
 
