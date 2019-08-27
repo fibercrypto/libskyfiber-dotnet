a8575a3695ad8b2d2285fdfe559cffec02c58bd5
Maykel Arias Torres <dev0006@simelo.tech>
2019-08-27 10:05:10 -0400

    [libskyapi] refs #65 Remove old sentence

--- lib/skyapi/mono_nunit_test.sh
+++ lib/skyapi/mono_nunit_test.sh
@@ -18,7 +18,6 @@ echo "[INFO] Install NUnit Console 3.x runners via NuGet"
 mono nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory packages
 
 echo "[INFO] Build the solution and run the unit test"
-ls -oa .
 msbuild Skyapi.sln
 
 mono ./packages/NUnit.Runners.2.6.4/tools/nunit-console.exe src/Skyapi.Test/bin/Debug/Skyapi.Test.dll
