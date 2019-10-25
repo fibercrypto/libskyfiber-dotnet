0c23e9a9034b1f9c847b75ec4108dcae25a4f864
Maykel Arias Torres <dev0006@simelo.tech>
2019-10-24 23:17:36 -0400

    [test-libsky] refs #72 Init suite test 'cipher.bip32', in `TestBip32TestVectors`

---
+++ lib/swig/LibskycoinNetTest/check_cipher_bip32.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using skycoin;
+namespace LibskycoinNetTest
+{
+    [TestFixture()]
+    public class check_cipher_bip32 : skycoin.skycoin
+    {
+        private const uint FirstHardenedChild = 0x80000000;
+        private struct testChildKey
+        {
+            string path;
+            string privKey;
+            string pubKey;
+            string fingerprint;
+            string identifier;
+            string chainCode;
+            string hexPubKey;
+            string wifPrivKey;
+            uint childNUmber;
+            char depth;
+        }
+
+        [Test()]
+        public void TestBip32TestVectors()
+        {
+
+        }
+    }
+}
